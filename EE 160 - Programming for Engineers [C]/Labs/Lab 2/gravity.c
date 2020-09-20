675ab5e4165aa73a5a612d546adf8f3b09f5cd6b
ethancheez <61472206+ethancheez@users.noreply.github.com>
2020-09-20 02:34:07 -1000

    Create gravity.c

---
+++ EE 160 - Programming for Engineers [C]/Labs/Lab 2/gravity.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() {  
+   float velocity, distance, time;
+ 
+        /*  set the time  */
+        time = 10;
+ 
+        /*  compute the velocity */
+        velocity = 32.0 * time;
+ 
+        /*  compute the distance  */
+        distance = 32 * time * time / 2;
+ 
+        /*  print the result  */
+        printf("An object falling %4.2f feet in %4.2f seconds ", distance, time);
+        printf("is traveling %4.2f ft/sec\n", velocity);
+}
