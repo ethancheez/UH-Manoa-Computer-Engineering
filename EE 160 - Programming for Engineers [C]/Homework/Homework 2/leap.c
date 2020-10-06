e87e7e10c9b3aeba810da76b2a227ae0a6708486
ethancheez <61472206+ethancheez@users.noreply.github.com>
2020-10-06 02:27:18 -1000

    Create leap.c

---
+++ EE 160 - Programming for Engineers [C]/Homework/Homework 2/leap.c
@@ -0,0 +1,13 @@
+#define FALSE 0
+#define TRUE 1
+
+int is_leap(int year){
+    if (year % 400 == 0)
+        return TRUE;
+    else if (year % 100 == 0)
+        return FALSE;
+    else if (year % 4 == 0)
+        return TRUE;
+    else
+        return FALSE;
+}
